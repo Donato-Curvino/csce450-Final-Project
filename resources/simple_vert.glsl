3af049cbc36a6f33598ebda8e90a5af3d1b9da57
Donato-Curvino <donato@tamu.edu>
2025-05-04 17:15:05 -0500

    update for opengl 3.2

--- resources/simple_vert.glsl
+++ resources/simple_vert.glsl
@@ -1,11 +1,14 @@
 #version 120
 
+attribute vec3 vertex;
+
 uniform mat4 P;
 uniform mat4 MV;
+
 varying vec3 vColor;
 
 void main()
 {
-	gl_Position = P * (MV * gl_Vertex);
-	vColor = gl_Color.rgb;
+        gl_Position = P * (MV * vec4(vertex, 1));
+        vColor = vec3(0, 0, 0);
 }
